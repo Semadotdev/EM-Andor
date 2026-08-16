e85e935ad1c9cad7d6aa4d044a1509288606b546
Semadotdev <jiroluis.bizz@gmail.com>
2026-08-16 19:03:00 +0800

    chore: add supabase, router, and vitest test harness

---
+++ src/test/setup.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom/vitest'
+
+class IntersectionObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+if (!globalThis.IntersectionObserver) {
+  globalThis.IntersectionObserver = IntersectionObserverMock
+}
